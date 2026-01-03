b2e65cbbb96d7488f4ce93fc0adda592115ebb59
sumitkr0308 <sumit2k30@gmail.com>
2026-01-03 18:37:29 +0530

    url fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token");
